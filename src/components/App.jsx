4ed25f20a50b7fe95d1a487373c30ea2ca93f20d
Evhen Kulbachenko <45nalitay45@gmail.com>
2024-09-19 12:12:27 +0300

    ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,14 +11,26 @@ import styles from './app.module.scss';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const newContacts = JSON.parse(localStorage.getItem('contacts-list'));
+
+    if (newContacts?.length) {
+      // newContacts && newContacts.length
+      this.setState({ contacts: newContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const contacts = this.state.contacts;
+    if (prevState.contacts.length !== contacts.length) {
+      localStorage.setItem('contacts-list', JSON.stringify(contacts));
+    }
+  }
+
   addContact = ({ name, number }) => {
     if (this.isDuplicated(name)) {
       Notify.failure(`${name} is already in contacts.`);
